dea0d935be142cb35453b94c640d6cd196a2dd6e
Sebastian Leidig <sebastian.leidig@gmail.com>
2015-09-28 16:18:21 +0200

    added "enrollments" to child (add/show school and class)

--- app/model/child.js
+++ app/model/child.js
@@ -3,7 +3,7 @@ angular.module('myApp.child', [
     'myApp.appDB',
 ])
 
-    .factory('Child', ['AbstractModel', function (AbstractModel) {
+    .factory('Child', ['AbstractModel', '$q', 'appDB', 'Enrollment', function (AbstractModel, $q, appDB, Enrollment) {
         var prefix = "child:";
 
         function Child(childData) {
@@ -16,6 +16,22 @@ angular.module('myApp.child', [
         Child.prefix = prefix;
 
         Child.prototype = angular.extend({}, AbstractModel, {
+            _poolEnrollment: {},
+            _retrieveEnrollment: function (id, data) {
+                var instance = this._poolEnrollment[id];
+
+                if (instance) {
+                    instance.setData(data);
+                } else {
+                    instance = new Enrollment(data);
+                    this._poolEnrollment[id] = instance;
+                }
+
+                return instance;
+            },
+
+
+            /* Public Methods */
             toString: function() {
                 return this.name+' ['+this.pn+']';
             },
@@ -27,7 +43,29 @@ angular.module('myApp.child', [
                 return Math.ceil(diff / (1000 * 3600 * 24 * 365));
             },
 
+
+            getEnrollments: function () {
+                var enrollmentPrefix = this._id+":enrollment:";
+                var deferred = $q.defer();
+                var scope = this;
+                appDB.allDocs({include_docs: true, startkey: enrollmentPrefix, endkey: enrollmentPrefix+"\ufff0"})
+                    .then(function (dataArray) {
+                        var items = [];
+                        dataArray.rows.forEach(function (row) {
+                            var data = row.doc;
+                            var o = scope._retrieveEnrollment(data._id, data);
+                            items.push(o);
+                        });
+
+                        deferred.resolve(items);
+                    },
+                    function (err) {
+                        deferred.reject();
+                    });
+                return deferred.promise;
+            },
         });
+
         return Child;
     }])
 
@@ -40,4 +78,49 @@ angular.module('myApp.child', [
         });
 
         return manager;
+    }])
+
+
+
+
+    .factory('Enrollment', ['AbstractModel', 'appDB', 'schoolManager', function (AbstractModel, appDB, schoolManager) {
+        function Enrollment(data) {
+            if (data) {
+                this.setData(data);
+                if(!this._id) {
+                    var prefix = data.child + ":enrollment:";
+                    var date = data.from.getFullYear()+"-"+data.from.getMonth()+"-"+data.from.getDate();
+                    this._id =  prefix + date;
+                }
+            }
+        };
+
+        Enrollment.prototype = angular.extend({}, AbstractModel, {
+            update: function () {
+                console.log(this);
+                var data = angular.copy(this);
+                data.school = this.school._id;
+
+                console.log(this);
+                appDB.put(data);
+                console.log(this);
+            },
+
+            setData: function (data) {
+                var scope = this;
+                if(typeof data.school === 'string') {
+                    schoolManager.get(data.school).then(
+                        function(school) {
+                            scope.school = school;
+                        }
+                    )
+                }
+
+                angular.extend(this, data);
+                return this;
+            },
+
+        });
+
+        return Enrollment;
     }]);
--- app/view/child/child.js
+++ app/view/child/child.js
@@ -63,79 +63,157 @@ angular.module('myApp.view.child', [
     }])
 
 
-    .controller('ChildDetailsController', ['$scope', '$location', '$log', '$routeParams', 'ngTableParams', 'childrenManager', 'Child', 'userManager', 'schoolManager',
-        function ($scope, $location, $log, $routeParams, ngTableParams, childrenManager, Child, userManager, schoolManager) {
-            var param = $routeParams.pn;
-            if (param === "new") {
-                $scope.child = {};
-                $scope.new = true;
-            }
-            else {
-                childrenManager.get(param).then(
-                    function (child) {
-                        $scope.child = child;
-
-                        $scope.tableFamily = new ngTableParams(
-                            {
-                                count: 25,
-                            },
-                            {
-                                getData: function ($defer, params) {
-                                    var data = [];
-                                    $scope.items = data;
-                                    params.total(data.length);
-                                    $defer.resolve(data);
-                                },
-                            }
-                        );
-
-                        $scope.tableCoaching = new ngTableParams(
-                            {
-                                count: 25,
-                            },
-                            {
-                                getData: function ($defer, params) {
-                                    var data = [];
-                                    $scope.items = data;
-                                    params.total(data.length);
-                                    $defer.resolve(data);
-                                },
-                            }
-                        );
-
-                        $scope.showFamilyMember = function (familyMemberId) {
-                            $location.path("/child/" + pn + "/family/" + familyMemberId);
-                        };
-                    },
-                    function (err) {
-                        $scope.error = "The given child could not be loaded.";
-
-                        $scope.child = {};
-                        $scope.new = true;
+    .controller('ChildDetailsController', ['$scope', '$sce', '$location', '$filter', '$log', '$routeParams', 'ngTableParams', 'childrenManager', 'Child', 'userManager', 'schoolManager', '$modal',
+    function ($scope, $sce, $location, $filter, $log, $routeParams, ngTableParams, childrenManager, Child, userManager, schoolManager, $modal) {
+        var loadEnrollments = function (child) {
+            $scope.tableEnrollments = new ngTableParams(
+                {
+                    count: 25,
+                    sorting: {
+                        from: 'desc'
                     }
-                );
+                },
+                {
+                    getData: function ($defer, params) {
+                        child.getEnrollments().then(
+                            function(data) {
+                                $scope.items = data;
+                                params.total(data.length);
+                                $defer.resolve($filter('orderBy')(data, params.orderBy()));
+                            },
+                            function(err) {
+                                $log.error("Could not load enrollment information. ("+err.message+")");
+                            }
+                        )
+                    },
+                }
+            );
+        }
+
+        var param = $routeParams.pn;
+        if (param === "new") {
+            $scope.child = {};
+            $scope.new = true;
+        }
+        else {
+            childrenManager.get(param).then(
+                function (child) {
+                    $scope.child = child;
+
+                    $scope.tableFamily = new ngTableParams(
+                        {
+                            count: 25,
+                        },
+                        {
+                            getData: function ($defer, params) {
+                                var data = [];
+                                $scope.items = data;
+                                params.total(data.length);
+                                $defer.resolve(data);
+                            },
+                        }
+                    );
+
+                    loadEnrollments(child);
+
+                    $scope.showFamilyMember = function (familyMemberId) {
+                        $location.path("/child/" + pn + "/family/" + familyMemberId);
+                    };
+                },
+                function (err) {
+                    $scope.error = "The given child could not be loaded.";
+
+                    $scope.child = {};
+                    $scope.new = true;
+                }
+            );
+        }
+
+
+        $scope.save = function () {
+            var child = $scope.child;
+            if ($scope.new) {
+                child = new Child(child);
+            }
+            child.update();
+        };
+
+        $scope.showEnrollment = function (enrollment) {
+            var msg = $sce.trustAsHtml('<p>These "enrollments" are designed to show the complete school life of the child. Please check carefully whether you really need to edit or delete this entry. ' +
+                    'Often it is more appropriate to add an additional entry through the "Update Class/School" button on the upper right of the section.</p>' +
+                    '<p><em>Edit only to correct errors. If the child did go to this school and class sometime, you should not edit or delete the entry but rather add a new one.</em></p>' +
+                    '<p>Continue to edit the entry?</p>');
+
+            var confirmScope = $scope.$new(true);
+            confirmScope.dialog = {
+                title: "Do you really want to edit?",
+                body: msg,
+            };
+            var confirmationModal = $modal.open({
+                animation: true,
+                templateUrl: 'view/confirmation-modal.html',
+                scope: confirmScope,
+            });
+            confirmationModal.result.then(function (res) {
+
+                var modalScope = $scope.$new(true);
+                modalScope.selectedEnrollment = enrollment;
+                modalScope.selectedChild = $scope.child;
+
+                var modalInstance = $modal.open({
+                    animation: false,
+                    templateUrl: 'view/child/enrollment-modal.html',
+                    controller: 'EnrollmentModalController',
+                    scope: modalScope,
+                });
+
+                modalInstance.result.then(function (res) {
+                    loadEnrollments($scope.child);
+                });
+            });
+        };
+
+
+        userManager.getAllSocialworkers().then(function (users) {
+            $scope.socialworkers = users;
+        });
+
+        $scope.centers = ['Tikiapara', 'Liluah'];
+
+    }])
+
+
+    .controller('EnrollmentModalController', ['$scope', '$modalInstance', 'schoolManager', 'Enrollment', function ($scope, $modalInstance, schoolManager, Enrollment) {
+        if($scope.selectedEnrollment === "new") {
+            $scope.selectedEnrollment = {};
+            $scope.newEnrollment = true;
+        }
+
+        $scope.saveEnrollment = function () {
+            //TODO: confirm
+
+            $scope.selectedEnrollment.child = $scope.selectedChild._id;
+            if($scope.newEnrollment) {
+                $scope.selectedEnrollment = new Enrollment($scope.selectedEnrollment);
             }
 
+            $scope.selectedEnrollment.update();
+            $modalInstance.close();
+        };
 
-            $scope.save = function () {
-                var child = $scope.child;
-                if ($scope.new) {
-                    child = new Child(child);
-                }
-                child.update();
-            };
+        $scope.deleteEnrollment = function () {
+            //TODO: confirm
+
+            $scope.selectedEnrollment.delete();
+            $modalInstance.close();
+        };
+        $scope.cancel = function () {
+            $modalInstance.dismiss();
+        };
 
 
-            userManager.getAllSocialworkers().then(function (users) {
-                $scope.socialworkers = users;
-            });
-
-            $scope.centers = ['Tikiapara', 'Liluah'];
-
-            schoolManager.getAll().then(function (schools) {
-                $scope.schools = schools;
-            });
-
-        }]);
-;
+        schoolManager.getAll().then(function (schools) {
+            $scope.schools = schools;
+        });
+    }]);
 
