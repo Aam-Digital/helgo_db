9d11d066326aa678225c8cb3c3ca216fffd63a08
Sebastian Leidig <sebastian.leidig@gmail.com>
2015-12-20 20:14:31 +0100

    fixed broken table sorting due to minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -357,7 +357,7 @@ module.exports = function (grunt) {
                 options: {
                     collapseWhitespace: true,
                     conservativeCollapse: true,
-                    collapseBooleanAttributes: true,
+                    collapseBooleanAttributes: false,
                     removeCommentsFromCDATA: true
                 },
                 files: [{
