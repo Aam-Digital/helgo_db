97bbb57b2fbeb2227bedaa80912708e1c3bc5b14
Frederik Schöll <fschoell@posteo.de>
2016-01-15 12:32:29 +0100

    code formatting

--- add-user.js
+++ add-user.js
@@ -4,7 +4,7 @@
  */
 
 /* display usage */
-if(['--help', '-help', '-h'].indexOf(process.argv[2]) > -1 || process.argv.length < 6 || process.argv.length > 7) {
+if (['--help', '-help', '-h'].indexOf(process.argv[2]) > -1 || process.argv.length < 6 || process.argv.length > 7) {
     console.log("Usage: " + process.argv[0] + " " + process.argv[1] + " admin:password database username password [user_rev]");
     console.log("If a user_rev is given, only the existing user in the helgo_db database is updated instead of creating a new user.");
     process.exit();
@@ -20,7 +20,7 @@ var database = process.argv[3];
 var username = process.argv[4];
 var password = process.argv[5];
 var rev;
-if(process.argv.length == 7) {
+if (process.argv.length == 7) {
     rev = process.argv[6];
 }
 
@@ -28,11 +28,11 @@ if(process.argv.length == 7) {
  * Encrypts the password to be saved into the helgo_db user database.
  */
 function encrypt(password) {
-    var cryptKeySize = 256/32;
+    var cryptKeySize = 256 / 32;
     var cryptIterations = 128;
-    var cryptSalt = CryptoJS.lib.WordArray.random(128/8).toString();
+    var cryptSalt = CryptoJS.lib.WordArray.random(128 / 8).toString();
     var hash = CryptoJS.PBKDF2(password, cryptSalt, {keySize: cryptKeySize, iterations: cryptIterations}).toString();
-    return { "hash":hash, "salt":cryptSalt, "iterations":cryptIterations, "keysize":cryptKeySize};
+    return {"hash": hash, "salt": cryptSalt, "iterations": cryptIterations, "keysize": cryptKeySize};
 }
 
 
@@ -44,7 +44,7 @@ function couchPut(dataPath, data) {
     var options = {
         hostname: 'localhost',
         port: 5984,
-        path: "/"+dataPath,
+        path: "/" + dataPath,
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
@@ -52,18 +52,18 @@ function couchPut(dataPath, data) {
         auth: adminAuth
     };
 
-    var req = http.request(options, function(res) {
+    var req = http.request(options, function (res) {
         console.log('STATUS: ' + res.statusCode);
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             console.log('RESPONSE: ' + chunk);
         });
-        res.on('end', function() {
+        res.on('end', function () {
             //console.log('No more data in response.')
         })
     });
 
-    req.on('error', function(e) {
+    req.on('error', function (e) {
         console.log('problem with request: ' + e.message);
     });
 
@@ -74,17 +74,17 @@ function couchPut(dataPath, data) {
 
 
 // Add CouchDB User
-if(!rev) {
-    var couchData = {'name':username, 'password':password, 'roles':['user_'+database], 'type':'user'};
+if (!rev) {
+    var couchData = {'name': username, 'password': password, 'roles': ['user_' + database], 'type': 'user'};
     // database is configured to be accessible for user role "user_$DB"
-    couchPut("_users/org.couchdb.user:"+username, couchData);
-    console.log("Adding new user '"+username+"' to CouchDB");
+    couchPut("_users/org.couchdb.user:" + username, couchData);
+    console.log("Adding new user '" + username + "' to CouchDB");
 }
 
 // Add helgo_db User in application database
-var hdbData = {'_id':'user:'+username,'name':username,'password':encrypt(password)};
-if(rev) {
+var hdbData = {'_id': 'user:' + username, 'name': username, 'password': encrypt(password)};
+if (rev) {
     hdbData._rev = rev;
 }
-couchPut(database+"/user:"+username, hdbData);
-console.log("Updating helgo_db database '"+database+"' for user '"+username+"'");
+couchPut(database + "/user:" + username, hdbData);
+console.log("Updating helgo_db database '" + database + "' for user '" + username + "'");
