f963b734821883f04b40304540c0dd6cc9195192
Sebastian Leidig <sebastian.leidig@gmail.com>
2016-01-28 17:53:41 +0100

    refactored appDB to have "interface" at beginning of the factory

--- app/scripts/services/appdb.js
+++ app/scripts/services/appdb.js
@@ -19,54 +19,59 @@ angular.module('hdbApp')
         pouchDBProvider.methods = angular.extend({}, POUCHDB_METHODS, authMethods);
     })
 
+
     .factory('appDB', ['$log', 'pouchDB', 'appConfig', function ($log, pouchDB, appConfig) {
-
-        var remoteDB = pouchDB(appConfig.database.remote_url + appConfig.database.name, {
-            skipSetup: true,
-            ajax: {rejectUnauthorized: false}
-        });
-
         var db = pouchDB(appConfig.database.name);
-        db.remoteDB = remoteDB;
 
-        db.login = function (username, password) {
+        db.remoteDB = setupRemoteDB();
+        db.sync = sync;
+        db.login = login;
+        db.logout = logout;
+
+        return db;
+
+
+        function setupRemoteDB() {
+            return pouchDB(appConfig.database.remote_url + appConfig.database.name, {
+                skipSetup: true,
+                ajax: {rejectUnauthorized: false}
+            });
+        }
+
+        function login(username, password) {
             var ajaxOpts = {
                 ajax: {
                     headers: {
-                        Authorization: 'Basic ' + window.btoa(username + ':' + password),
+                        Authorization: 'Basic ' + window.btoa(username + ':' + password)
                     }
                 }
             };
-            return db.remoteDB.login(username, password, ajaxOpts)
-                .then(function () {
-                        $log.debug("Remote login successful.");
-                    },
-                    function (error) {
-                        $log.error("Could not log in to the remote database. (" + error.message + ")");
-                    });
-        };
 
-        db.sync = function (syncLive) {
-            return PouchDB.sync(db, remoteDB, {
-                live: syncLive,
-                retry: syncLive
-            }).then(
+            return db.remoteDB.login(username, password, ajaxOpts).then(
+                function () {
+                    $log.debug("Remote login successful.");
+                },
+                function (error) {
+                    $log.error("Could not log in to the remote database. (" + error.message + ")");
+                });
+        }
+
+        function sync(syncLive) {
+            return PouchDB.sync(db, db.remoteDB, {live: syncLive, retry: syncLive}).then(
                 function () {
                     $log.debug("sync successfully");
-                }, function (err) {
+                },
+                function (err) {
                     $log.debug("sync failed:");
                     $log.debug(err);
                 },
                 function (notify) {
                     $log.debug("sync notification:");
                     $log.debug(notify);
-                }
-            );
-        };
+                });
+        }
 
-        db.logout = function () {
+        function logout() {
             return db.remoteDB.logout();
-        };
-
-        return db;
+        }
     }]);
