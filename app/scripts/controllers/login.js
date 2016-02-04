a99bb4792ab9531b01700840e5e365f0dcb87510
Frederik Schöll <fschoell@posteo.de>
2016-02-04 14:22:37 +0100

    auto format for merging

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,88 +8,87 @@
  * Controller of the hdbApp
  */
 angular.module('hdbApp')
-    .controller('LoginCtrl', ['$scope', '$analytics', '$location', '$log', 'userManager', 'appDB', 'cookie',
-        function ($scope, $analytics, $location, $log, userManager, appDB, cookie) {
+    .controller('LoginCtrl', ['$scope', '$analytics', '$location', '$log', 'userManager', 'appDB', 'cookie', function ($scope, $analytics, $location, $log, userManager, appDB, cookie) {
 
-            $scope.isLoginBtnDisabled = false;
+        $scope.isLoginBtnDisabled = false;
 
-            $scope.login = function login() {
-                $scope.isLoginBtnDisabled = true;
+        $scope.login = function login() {
+            $scope.isLoginBtnDisabled = true;
 
-                if (!$scope.user.password) {
-                    $scope.user.password = "";
-                }
+            if (!$scope.user.password) {
+                $scope.user.password = "";
+            }
 
-                // try the local login first
-                userManager.login($scope.user.name, $scope.user.password).then(
-                    function (status) {
-                        if (status.ok) {
-                            $log.debug("Local login successful.");
-                            appDB.login($scope.user.name, $scope.user.password).then(
-                                function () {
-                                    // local login successful, replicate the database and activate live sync
-                                    appDB.sync().then(
-                                        // if sync failed, check if database is outdated
-                                        function (err) {
-                                            if (appDB.isOutdated()) {
-                                                // TODO replace logging with alert
-                                                $log.debug("Database is outdated, please go online to synchronize");
-                                            }
-                                        });
-                                }, function (err) {
-                                    $log.debug("Remote login failed: ");
-                                    $log.debug(err);
-                                }
-                            );
-                            $analytics.eventTrack($scope.user.name, {category: 'user', label: $scope.user.name});
-                            $location.path("/");
-                        } else {
-                            _loginFailed();
-                        }
-                    }, function () {
+            // try the local login first
+            userManager.login($scope.user.name, $scope.user.password).then(
+                function (status) {
+                    if (status.ok) {
+                        $log.debug("Local login successful.");
+                        appDB.login($scope.user.name, $scope.user.password).then(
+                            function () {
+                                // local login successful, replicate the database and activate live sync
+                                appDB.sync().then(
+                                    // if sync failed, check if database is outdated
+                                    function (err) {
+                                        if (appDB.isOutdated()) {
+                                            // TODO replace logging with alert
+                                            $log.debug("Database is outdated, please go online to synchronize");
+                                        }
+                                    });
+                            }, function (err) {
+                                $log.debug("Remote login failed: ");
+                                $log.debug(err);
+                            }
+                        );
+                        $analytics.eventTrack($scope.user.name, {category: 'user', label: $scope.user.name});
+                        $location.path("/");
+                    } else {
                         _loginFailed();
                     }
-                );
+                }, function () {
+                    _loginFailed();
+                }
+            );
 
-                function _loginFailed() {
-                    $log.debug("Local login failed, is a local database available?");
+            function _loginFailed() {
+                $log.debug("Local login failed, is a local database available?");
 
-                    appDB.login($scope.user.name, $scope.user.password).then(
-                        function () {
-                            $log.debug("Remote login successful, trying to sync the database...");
-                            appDB.sync().then(
-                                function () {
-                                    userManager.login($scope.user.name, $scope.user.password).then(
-                                        function (status) {
-                                            if (status.ok) {
-                                                $log.debug("Local login successful.");
-                                                $location.path("/");
-                                            }
-                                            else {
-                                                $scope.error = status.message;
-                                                $scope.isLoginBtnDisabled = false;
-                                            }
-                                        },
-                                        function (err) {
-                                            $scope.error = err.message;
+                appDB.login($scope.user.name, $scope.user.password).then(
+                    function () {
+                        $log.debug("Remote login successful, trying to sync the database...");
+                        appDB.sync().then(
+                            function () {
+                                userManager.login($scope.user.name, $scope.user.password).then(
+                                    function (status) {
+                                        if (status.ok) {
+                                            $log.debug("Local login successful.");
+                                            $location.path("/");
+                                        }
+                                        else {
+                                            $scope.error = status.message;
                                             $scope.isLoginBtnDisabled = false;
                                         }
-                                    )
-                                },
-                                function (err) {
-                                    $scope.error = err.message;
-                                    $scope.isLoginBtnDisabled = false;
-                                }
-                            )
-                        },
-                        function (err) {
-                            $scope.error = err.message;
-                            $scope.isLoginBtnDisabled = false;
-                        }
-                    );
-                }
+                                    },
+                                    function (err) {
+                                        $scope.error = err.message;
+                                        $scope.isLoginBtnDisabled = false;
+                                    }
+                                )
+                            },
+                            function (err) {
+                                $scope.error = err.message;
+                                $scope.isLoginBtnDisabled = false;
+                            }
+                        )
+                    },
+                    function (err) {
+                        $scope.error = err.message;
+                        $scope.isLoginBtnDisabled = false;
+                    }
+                );
+            }
 
-            };
-        }]);
+        };
+    }]);
 
 
