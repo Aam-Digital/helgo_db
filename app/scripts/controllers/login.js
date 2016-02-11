651ae94d3aee09dd897886166a4139a824147dc6
Sebastian Leidig <sebastian.leidig@gmail.com>
2016-02-11 23:09:36 +0100

    refactored LoginCtrl and moved appDB.isOutdated check to correct place

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,114 +10,125 @@
 angular.module('hdbApp')
     .controller('LoginCtrl', ['$scope', '$analytics', '$location', '$log', '$uibModal', 'userManager', 'appDB', 'latestChanges', 'alertManager',
         function ($scope, $analytics, $location, $log, $uibModal, userManager, appDB, latestChanges, alertManager) {
-
             $scope.isLoginBtnDisabled = false;
+            $scope.user = {name: "", password: ""};
+            $scope.login = login;
             var modal;
 
-            $scope.login = function login() {
+            function login() {
                 $scope.isLoginBtnDisabled = true;
-
-                if (!$scope.user.password) {
-                    $scope.user.password = "";
-                }
 
                 // try the local login first
                 userManager.login($scope.user.name, $scope.user.password).then(
                     function (status) {
                         if (status.ok) {
-                            $log.debug("Local login successful.");
-                            appDB.login($scope.user.name, $scope.user.password).then(
-                                function () {
-                                    // local login successful, replicate the database and activate live sync
-                                    appDB.sync().then(
-                                        // if sync failed, check if database is outdated
-                                        function (err) {
-                                            if (appDB.isOutdated()) {
-                                                alertManager.addAlert('You are working on an outdated database, please ' +
-                                                    'online as soon as possible to synchronize the database!', alertManager.ALERT_WARNING);
-                                            }
-                                            $log.debug(err);
-                                            // TODO show info icon: you are working offline?
-                                        });
-                                }, function (err) {
-                                    $log.debug("Remote login failed: ");
-                                    $log.debug(err);
-                                }
-                            );
-                            onLoginComplete();
+                            _localLoginSuccessful();
                         } else {
-                            _localLoginFailed();
+                            _tryRemoteDatabaseDownload();
                         }
                     }, function () {
-                        _localLoginFailed();
+                        _tryRemoteDatabaseDownload();
+                    }
+                );
+            }
+
+
+            function _localLoginSuccessful() {
+                $log.debug("Local login successful.");
+                appDB.login($scope.user.name, $scope.user.password).then(
+                    function () {
+                        // local login successful, replicate the database and activate live sync
+                        appDB.sync()
+                            .catch(function () {
+                                checkAndWarnOutdatedLocalDatabase();
+                            });
+                    }, function (err) {
+                        $log.debug("Remote login failed: ");
+                        $log.debug(err);
+                        checkAndWarnOutdatedLocalDatabase();
                     }
                 );
 
-                // local login using the database failed
-                function _localLoginFailed() {
-                    $log.debug("Local login failed, is a local database available?");
-                    showDownloadProgress();
+                onLoginComplete();
+            }
 
-                    // try remote login
-                    appDB.login($scope.user.name, $scope.user.password).then(
-                        function () {
-                            $log.debug("Remote login successful, trying to sync the database...");
-                            appDB.sync().then(
-                                function () {
-                                    userManager.login($scope.user.name, $scope.user.password).then(
-                                        function (status) {
-                                            if (status.ok) {
-                                                $log.debug("Local login successful.");
-                                                onLoginComplete();
-                                            }
-                                            else {
-                                                $scope.error = status.message;
-                                                $scope.isLoginBtnDisabled = false;
-                                            }
-                                        },
-                                        function (err) {
-                                            $scope.error = err.message;
-                                            $scope.isLoginBtnDisabled = false;
-                                        }
-                                    );
-                                    modal.close();
-                                },
-                                function (err) {
-                                    $scope.error = err.message;
-                                    $scope.isLoginBtnDisabled = false;
-                                    modal.close();
+            // local login using the database failed
+            function _tryRemoteDatabaseDownload() {
+                $log.debug("Local login failed, is a local database available?");
+                showDownloadProgress();
+
+                // try remote login
+                appDB.login($scope.user.name, $scope.user.password).then(
+                    function () {
+                        _remoteLoginSuccessful();
+                    },
+                    function (err) {
+                        $scope.error = err.message;
+                        $scope.isLoginBtnDisabled = false;
+                        modal.close();
+                    }
+                );
+            }
+
+            function showDownloadProgress() {
+                modal = $uibModal.open({
+                    animation: true,
+                    templateUrl: 'views/download-progress.html',
+                    controller: 'LoginCtrl'
+                });
+            }
+
+            function _remoteLoginSuccessful() {
+                $log.debug("Remote login successful, trying to sync the database...");
+                appDB.sync().then(
+                    function () {
+                        userManager.login($scope.user.name, $scope.user.password).then(
+                            function (status) {
+                                if (status.ok) {
+                                    $log.debug("Local login successful.");
+                                    onLoginComplete();
                                 }
-                            )
-                        },
-                        function (err) {
-                            $scope.error = err.message;
-                            $scope.isLoginBtnDisabled = false;
-                            modal.close();
-                        }
-                    );
+                                else {
+                                    $scope.error = status.message;
+                                    $scope.isLoginBtnDisabled = false;
+                                }
+                            },
+                            function (err) {
+                                $scope.error = err.message;
+                                $scope.isLoginBtnDisabled = false;
+                            }
+                        );
+                        modal.close();
+                    },
+                    function (err) {
+                        $scope.error = err.message;
+                        $scope.isLoginBtnDisabled = false;
+                        modal.close();
+                    }
+                )
+            }
+
+            function checkAndWarnOutdatedLocalDatabase() {
+                if (appDB.isOutdated()) {
+                    alertManager.addAlert('You are working on an outdated database, please ' +
+                        'online as soon as possible to synchronize the database!', alertManager.ALERT_WARNING);
                 }
+                // TODO show info icon: you are working offline?
+            }
 
-                function onLoginComplete() {
-                    $location.path("/");
 
-                    $analytics.eventTrack($scope.user.name, {category: 'user', label: $scope.user.name});
+            function onLoginComplete() {
+                $location.path("/");
 
-                    var user = userManager.getCurrentUser();
-                    latestChanges.check(user.settings.lastKnownVersion).then(function (res) {
-                        if (res) {
-                            user.settings.lastKnownVersion = res;
-                            user.update();
-                            $log.info("Updated to new version.");
-                        }
-                    });
-                }
+                $analytics.eventTrack($scope.user.name, {category: 'user', label: $scope.user.name});
 
-                function showDownloadProgress() {
-                    modal = $uibModal.open({
-                        animation: true,
-                        templateUrl: 'views/download-progress.html',
-                        controller: 'LoginCtrl'
-                    });
-                }
-            };
+                var user = userManager.getCurrentUser();
+                latestChanges.check(user.settings.lastKnownVersion).then(function (res) {
+                    if (res) {
+                        user.settings.lastKnownVersion = res;
+                        user.update();
+                        $log.info("Updated to new version.");
+                    }
+                });
+            }
         }]);
